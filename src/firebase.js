7fa569ca29c1ece39e782e3c9aa2ea401d9f2b3d
Christain Barnes <chrisbarnes01@live.com>
2023-05-03 23:57:00 -0400

    og files

---
+++ src/firebase.js
@@ -0,0 +1,103 @@
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBf9OKh0PyMuWoUmxEq25ILSnHEeG7zZxk",
+  authDomain: "the-wrx.firebaseapp.com",
+  projectId: "the-wrx",
+  storageBucket: "the-wrx.appspot.com",
+  messagingSenderId: "749742144913",
+  appId: "1:749742144913:web:2900f3e7150f8881fe6e30",
+  measurementId: "G-6F3L18LY7H",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  auth,
+  logInWithEmailAndPassword,
+  signInWithGoogle,
+} from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./Login.css";
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user) navigate("/dashboard");
+  }, [user, loading]);
+
+  return (
+    <div>
+      <div className="center">
+        <h1>The Wrx</h1>
+        <div class="row">
+          <div class="column">
+            <h3>Live</h3>
+
+            <img
+              src={require("../../assets/live.png")}
+              style={{ width: "100%", height: "100%" }}
+              alt="live"
+            />
+          </div>
+          <div class="column">
+            <h3>Give</h3>
+
+            <img
+              src={require("../../assets/give.png")}
+              style={{ width: "100%", height: "100%" }}
+              alt="give"
+            />
+          </div>
+          <div class="column">
+            <h3>Love</h3>
+            <img
+              src={require("../../assets/love.png")}
+              style={{ width: "100%", height: "100%" }}
+              alt="love"
+            />
+          </div>
+        </div>
+        <p>The Wrx is a collaborative tool built to help you track how you live a good life. Join the beta today!</p>
+      </div>
+      <div className="login">
+        <div className="login__container">
+          <input
+            type="text"
+            className="login__textBox"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="E-mail Address"
+          />
+          <input
+            type="password"
+            className="login__textBox"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button
+            className="login__btn"
+            onClick={() => logInWithEmailAndPassword(email, password)}
+          >
+            Login
+          </button>
+          <button
+            className="login__btn login__google"
+            onClick={signInWithGoogle}
+          >
+            Login with Google
+          </button>
+          <div>
+            <Link to="/reset">Forgot Password</Link>
+          </div>
+          <div>
+            Don't have an account? <Link to="/register">Register</Link> now.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Login;
